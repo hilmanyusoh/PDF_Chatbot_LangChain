5485d72074d07b351c6c57068742d0c6aac493fb
Hilman Yusoh <118374893+hilmanyusoh@users.noreply.github.com>
2025-05-28 13:53:24 +0700

    Update qa_system.py

--- collect_data/qa_system.py
+++ collect_data/qa_system.py
@@ -1,18 +1,21 @@
 import faiss
 import pickle
 from sentence_transformers import SentenceTransformer
+import torch
 
 class HadithQA:
     def __init__(self):
         # Load FAISS index
-        self.index = faiss.read_index("hadith_faiss_index/index.faiss")  # ตรวจสอบ path ให้ถูกต้อง
+        self.index = faiss.read_index("hadith_faiss_index/index.faiss")
         
         # Load chunks
         with open("hadith_chunks.pkl", "rb") as f:
             self.chunks = pickle.load(f)
 
+        # Select device
+        device = "cuda" if torch.cuda.is_available() else "cpu"
         # Load embedding model
-        self.embedding_model = SentenceTransformer("BAAI/bge-small-en-v1.5")
+        self.embedding_model = SentenceTransformer("BAAI/bge-small-en-v1.5", device=device)
 
     def ask_question(self, question, k=3):
         q_emb = self.embedding_model.encode([question], normalize_embeddings=True)
